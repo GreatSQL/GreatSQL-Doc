9b6fb857d64ecf39682b3809cd8bbba2192c9ca4
Ye Jinrong <jinrong.ye@greatdb.com>
2022-07-18 10:24:42 +0800

    [new]greatsql_docker_build

---
+++ greatsql_docker_build/greatsql-docker-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker build -t greatsql_build_env .
+
+src_dir=$1
+
+if [ $? -ne 0 ];then
+  echo "Docker build error!"
+else 
+  echo "Docker build success!you can run it:
+
+docker run -d -v $src_dir:/opt/greatsql-8.0.25-16 greatsql_build_env"
+fi
