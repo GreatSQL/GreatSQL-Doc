bba7e86805e02d18469861f430b0b0772459b9ec
Ye Jinrong <jinrong.ye@greatdb.com>
2023-12-04 17:29:17 +0800

    批量导入脚本中增加指定数据库名

--- tpch/3.0.1/load-data-parallel.sh
+++ tpch/3.0.1/load-data-parallel.sh
@@ -13,11 +13,11 @@ MYSQL_CLI="mysql -h$host -P$port -u$user -p'$passwd' -f ${tpchdb}"
 # 需要先修改GreatSQL选项`secure_file_priv`设置，指向上述 workdir 所在目录，重启GreatSQL使之生效
 # 可通过修改下面SQL命令中的参数`gdb_parallel_load_workers=16`调整导入的并发线程数
 
-$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/region.tbl' into table region FIELDS TERMINATED BY '|'; " &
-$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/nation.tbl' into table nation FIELDS TERMINATED BY '|'; " &
-$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/supplier.tbl' into table supplier FIELDS TERMINATED BY '|'; " &
-$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/part.tbl' into table part FIELDS TERMINATED BY '|'; " &
-$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/customer.tbl' into table customer FIELDS TERMINATED BY '|'; " &
-$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/partsupp.tbl' into table partsupp FIELDS TERMINATED BY '|'; " &
-$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/orders.tbl' into table orders FIELDS TERMINATED BY '|'; " &
-$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/lineitem.tbl' into table lineitem FIELDS TERMINATED BY '|'; " &
+$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/region.tbl' into table region FIELDS TERMINATED BY '|'; " ${tpchdb} &
+$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/nation.tbl' into table nation FIELDS TERMINATED BY '|'; " ${tpchdb} &
+$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/supplier.tbl' into table supplier FIELDS TERMINATED BY '|'; " ${tpchdb} &
+$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/part.tbl' into table part FIELDS TERMINATED BY '|'; " ${tpchdb} &
+$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/customer.tbl' into table customer FIELDS TERMINATED BY '|'; " ${tpchdb} &
+$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/partsupp.tbl' into table partsupp FIELDS TERMINATED BY '|'; " ${tpchdb} &
+$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/orders.tbl' into table orders FIELDS TERMINATED BY '|'; " ${tpchdb} &
+$MYSQL_CLI -f -e "load /*+ SET_VAR(gdb_parallel_load=ON) SET_VAR(gdb_parallel_load_workers=16)*/ data infile '${workdir}/lineitem.tbl' into table lineitem FIELDS TERMINATED BY '|'; " ${tpchdb} &
