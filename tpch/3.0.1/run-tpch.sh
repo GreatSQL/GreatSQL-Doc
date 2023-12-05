43283f2623f9cf3b0f0c0c0646529cbb3676068b
Ye Jinrong <jinrong.ye@greatdb.com>
2023-12-05 16:22:42 +0800

    增加logdir变量，提升脚本灵活性

--- tpch/3.0.1/run-tpch.sh
+++ tpch/3.0.1/run-tpch.sh
@@ -5,10 +5,11 @@ host="172.16.16.10"
 port="3306"
 user="tpch"
 passwd="tpch"
+logdir="log"
 
 cd ${workdir}
-mkdir -p log
-MYSQL_CLI="mysql -h$host -P$port -u$user -p'$passwd' -f ${tpchdb}"
+mkdir -p ${logdir}
+MYSQL_CLI="mysql -h"${host}" -P"${port}" -u"${user}" -p"${passwd}" -f ${tpchdb}"
 
 # 第一遍执行，先预热数据
 for i in $(seq 1 22)
@@ -26,15 +27,15 @@ do
  do
 
    time_1=`date +%s%N`
-	 echo `date  '+[%Y-%m-%d %H:%M:%S]'` "BEGIN RUN TPC-H Q${i} ${j} times" >> ./log/run-tpch-queries.log 2>&1
+	 echo `date  '+[%Y-%m-%d %H:%M:%S]'` "BEGIN RUN TPC-H Q${i} ${j} times" >> ./${logdir}/run-tpch-queries.log 2>&1
 
 	 echo "RUN TPC-H Q${i} ${j} times"
-	 $MYSQL_CLI < ./queries/tpch_queries_$i.sql >> ./log/tpch_queries_$i.res 2>&1
+	 $MYSQL_CLI < ./queries/tpch_queries_$i.sql >> ./${logdir}/tpch_queries_$i.res 2>&1
 
 	 time_2=`date +%s%N`
-	 durtime=`echo $time_2 $time_1 | awk '{printf "%0.2f\n", ($1 - $2) / 1000000000}'`
-	 echo `date  '+[%Y-%m-%d %H:%M:%S]'` "TPC-H Q${i} END, COST: ${durtime}s" >> ./log/run-tpch-queries.log 2>&1
-	 echo "" >> ./log/run-tpch-queries.log 2>&1
-	 echo "" >> ./log/run-tpch-queries.log 2>&1
+	 durtime=`echo $time_2 $time_1 | awk '{printf "%0.3f\n", ($1 - $2) / 1000000000}'`
+	 echo `date  '+[%Y-%m-%d %H:%M:%S]'` "TPC-H Q${i} END, COST: ${durtime}s" >> ./${logdir}/run-tpch-queries.log 2>&1
+	 echo "" >> ./${logdir}/run-tpch-queries.log 2>&1
+	 echo "" >> ./${logdir}/run-tpch-queries.log 2>&1
  done
 done
