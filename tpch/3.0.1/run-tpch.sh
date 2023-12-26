3e998f572c760055e4ce26d0cb373976ddd11832
Ye Jinrong <jinrong.ye@greatdb.com>
2023-12-26 13:01:30 +0800

    修改运行方式，每条SQL预热完后立即执行，而不是所有SQL全部预热完后才逐条执行

--- tpch/3.0.1/run-tpch.sh
+++ tpch/3.0.1/run-tpch.sh
@@ -5,37 +5,34 @@ host="172.16.16.10"
 port="3306"
 user="tpch"
 passwd="tpch"
-logdir="log"
+logdir="tpch-runlog-`date +%Y%m%d`"
 
 cd ${workdir}
 mkdir -p ${logdir}
 MYSQL_CLI="mysql -h"${host}" -P"${port}" -u"${user}" -p"${passwd}" -f ${tpchdb}"
 
-# 第一遍执行，先预热数据
+# 每个查询SQL执行5遍，其中前2遍是预热
 for i in $(seq 1 22)
 do
- $MYSQL_CLI < ./queries/tpch_queries_$i.sql > /dev/null 2>&1
- echo "tpch_queries_$i.sql warmup ended"
-done
-sleep 30
-echo "sleeping for 30 seconds"
-
-# 正式测试，每个查询SQL执行3遍
-for i in $(seq 1 22)
-do
- for j in $(seq 1 3)
+ for j in $(seq 1 5)
  do
+   if [ ${j} -le 2 ] ; then
+     $MYSQL_CLI < ./queries/tpch_queries_$i.sql > /dev/null 2>&1
+     echo "tpch_queries_$i.sql warmup ${j} times ended"
+     echo "sleeping for 5 seconds"
+     sleep 5
+   else
+     time_1=`date +%s%N`
+     echo `date  '+[%Y-%m-%d %H:%M:%S]'` "BEGIN RUN TPC-H Q${i} ${j} times" >> ./${logdir}/run-tpch-queries.log 2>&1
 
-   time_1=`date +%s%N`
-	 echo `date  '+[%Y-%m-%d %H:%M:%S]'` "BEGIN RUN TPC-H Q${i} ${j} times" >> ./${logdir}/run-tpch-queries.log 2>&1
+     echo "RUN TPC-H Q${i} ${j} times"
+     $MYSQL_CLI < ./queries/tpch_queries_$i.sql >> ./${logdir}/tpch_queries_$i.res 2>&1
 
-	 echo "RUN TPC-H Q${i} ${j} times"
-	 $MYSQL_CLI < ./queries/tpch_queries_$i.sql >> ./${logdir}/tpch_queries_$i.res 2>&1
-
-	 time_2=`date +%s%N`
-	 durtime=`echo $time_2 $time_1 | awk '{printf "%0.3f\n", ($1 - $2) / 1000000000}'`
-	 echo `date  '+[%Y-%m-%d %H:%M:%S]'` "TPC-H Q${i} END, COST: ${durtime}s" >> ./${logdir}/run-tpch-queries.log 2>&1
-	 echo "" >> ./${logdir}/run-tpch-queries.log 2>&1
-	 echo "" >> ./${logdir}/run-tpch-queries.log 2>&1
+     time_2=`date +%s%N`
+     durtime=`echo $time_2 $time_1 | awk '{printf "%0.3f\n", ($1 - $2) / 1000000000}'`
+     echo `date  '+[%Y-%m-%d %H:%M:%S]'` "TPC-H Q${i} END, COST: ${durtime}s" >> ./${logdir}/run-tpch-queries.log 2>&1
+     echo "" >> ./${logdir}/run-tpch-queries.log 2>&1
+     echo "" >> ./${logdir}/run-tpch-queries.log 2>&1
+   fi
  done
 done
